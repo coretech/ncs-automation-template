3da4e5b64aef5bfe0fbc229629b5f2ea33ff4950
OlegTagayIDT <oleg.tagay@idt.net>
2022-11-01 13:30:00 +0100

    init

---
+++ src/main/java/com/n2p/EntitesPreparationApplication.java
@@ -0,0 +1,13 @@
+package com.n2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntitesPreparationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntitesPreparationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/n2p/EntitesPreparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntitesPreparationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
